dbe87ffca74afbe6a625593f4feb2b5d8646c929
Geopopos <groros2@icloud.com>
2020-03-30 21:28:49 -0400

    added opponent with basic AI functionality

---
+++ Opponent/Opponent.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+export var speed = 200
+var screensize
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+
+func _process(delta):
+	var velocity = Vector2()
+	var ball = get_parent().get_node("Ball")
+	if ball:
+		if ball.position.y > (position.y - 16):
+			velocity.y += 1
+		elif ball.position.y < (position.y + 16):
+			velocity.y -= 1
+		if velocity.length() > 0:
+			velocity *= speed
+		position += velocity * delta
+		position.y = clamp(position.y, 0, screensize.y)
+
