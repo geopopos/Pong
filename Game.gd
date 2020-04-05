482d47a3532a80eca7fa4390b6003f2e1d1dca9b
Geopopos <groros2@icloud.com>
2020-04-05 15:00:01 -0400

    added bricks to game

--- Game.gd
+++ Game.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
+export (PackedScene) var Brick 
+
 export var max_score: int = 7
 export var player1_score: int
 export var player2_score: int
 var winner
 var ball
+var screensize
 
 
 func _ready():
 	randomize()
+	screensize = get_viewport_rect().size
 
 func game_start():
 	player1_score = 0
 	player2_score = 0
 	$HUD.update_score("Score1Label", player1_score)
 	$HUD.update_score("Score2Label", player2_score)
+	populate_bricks()
 	$HUD.show_message("Get Ready")
 	yield(get_tree().create_timer(3.0), "timeout")
 	add_child(load("res://Ball/Ball.tscn").instance())
@@ -51,3 +56,18 @@ func check_win():
 		winner = "Player 2"
 		return true
 	return false
+
+func populate_bricks():
+	var brick_pos = Vector2(48, 16)
+	for i in range(screensize.y/(40)):
+		var brick = Brick.instance()
+		add_child(brick)
+		brick.position = brick_pos
+		brick_pos.y +=40 
+
+	brick_pos = Vector2(800, 16)
+	for i in range(screensize.y/(40)):
+		var brick = Brick.instance()
+		add_child(brick)
+		brick.position = brick_pos
+		brick_pos.y +=40 
